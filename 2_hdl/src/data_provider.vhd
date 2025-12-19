b183b589cc8761f4175c47d8c63c52dfa592f66e
Timon Burkard <burkard.timon@gmail.com>
2025-12-19 10:04:55 +0100

    [vivado/rtl] data_provider: provide data over AXI4-Stream when position changes
    The data sent over AXI4-Stream is sign-extended to 32 bit. So that the
    DMA can always use 32 bit.
    
    Signed-off-by: Timon Burkard <burkard.timon@gmail.com>

--- 2_hdl/src/data_provider.vhd
+++ 2_hdl/src/data_provider.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity Data_Provider is
     Generic (
@@ -21,7 +22,46 @@ entity Data_Provider is
 end Data_Provider;
 
 architecture Behavioral of Data_Provider is
+    signal tdata_reg  : STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
+    signal tvalid_reg : STD_LOGIC := '0';
+    signal tlast_reg  : STD_LOGIC := '0';
+    signal prev_pos   : STD_LOGIC_VECTOR (DATA_WIDTH-1 downto 0) := (others => '0');
+    signal has_data   : STD_LOGIC := '0';
 begin
-    -- Implementation placeholder
-    position_available <= '0';
+    -- Present data on AXIS when `position` changes.
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then
+                tdata_reg  <= (others => '0');
+                tvalid_reg <= '0';
+                tlast_reg  <= '0';
+                prev_pos   <= (others => '0');
+                has_data   <= '0';
+                position_available <= '0';
+            else
+                -- capture new position and start transfer
+                if (position /= prev_pos) and (has_data = '0') then
+                    -- sign-extend `position` to 32 bits
+                    tdata_reg  <= std_logic_vector(resize(signed(position), 32));
+                    tvalid_reg <= '1';
+                    tlast_reg  <= '1';
+                    has_data   <= '1';
+                    position_available <= '1';
+                    prev_pos <= position;
+                elsif (tvalid_reg = '1') and (m_axis_tready = '1') then
+                    -- transfer accepted by slave
+                    tvalid_reg <= '0';
+                    tlast_reg  <= '0';
+                    has_data   <= '0';
+                    position_available <= '0';
+                end if;
+            end if;
+        end if;
+    end process;
+
+    m_axis_tdata  <= tdata_reg;
+    m_axis_tvalid <= tvalid_reg;
+    m_axis_tlast  <= tlast_reg;
+
 end Behavioral;
